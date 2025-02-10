3176c1a83c847fdb5459abd0d20e1827c5cb11df
MikaelHepner <101403839+Derwinesmitch@users.noreply.github.com>
2025-02-10 23:09:02 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import FrontPage from './pages/frontpage'
-
+// import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
